72c8aa675d6118907a0279a915bbdfd30fd714a9
OratileMmamaropengMa <OratileMmamaropengMa@RFCPTLAB4PC12>
2024-06-04 11:02:16 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,392 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtmarks = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnPassOrFail = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtPeriodTime = New System.Windows.Forms.TextBox()
+        Me.btnPay = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.cmbDAyWeek = New System.Windows.Forms.ComboBox()
+        Me.cmbMonth = New System.Windows.Forms.ComboBox()
+        Me.txtname = New System.Windows.Forms.TextBox()
+        Me.lblname = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtheight = New System.Windows.Forms.TextBox()
+        Me.txtwidth = New System.Windows.Forms.TextBox()
+        Me.txtDepth = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtmarks
+        '
+        Me.txtmarks.Location = New System.Drawing.Point(110, 42)
+        Me.txtmarks.Name = "txtmarks"
+        Me.txtmarks.Size = New System.Drawing.Size(100, 20)
+        Me.txtmarks.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter Mark "
+        '
+        'btnPassOrFail
+        '
+        Me.btnPassOrFail.Location = New System.Drawing.Point(49, 78)
+        Me.btnPassOrFail.Name = "btnPassOrFail"
+        Me.btnPassOrFail.Size = New System.Drawing.Size(75, 23)
+        Me.btnPassOrFail.TabIndex = 2
+        Me.btnPassOrFail.Text = "Pass Or Fail"
+        Me.btnPassOrFail.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "time spent "
+        '
+        'txtPeriodTime
+        '
+        Me.txtPeriodTime.Location = New System.Drawing.Point(135, 49)
+        Me.txtPeriodTime.Name = "txtPeriodTime"
+        Me.txtPeriodTime.Size = New System.Drawing.Size(100, 20)
+        Me.txtPeriodTime.TabIndex = 4
+        '
+        'btnPay
+        '
+        Me.btnPay.Location = New System.Drawing.Point(9, 89)
+        Me.btnPay.Name = "btnPay"
+        Me.btnPay.Size = New System.Drawing.Size(75, 23)
+        Me.btnPay.TabIndex = 5
+        Me.btnPay.Text = "Pay"
+        Me.btnPay.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(90, 89)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(171, 89)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "Exit "
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.GroupBox1.Controls.Add(Me.Label10)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.btnExit)
+        Me.GroupBox1.Controls.Add(Me.txtPeriodTime)
+        Me.GroupBox1.Controls.Add(Me.btnReset)
+        Me.GroupBox1.Controls.Add(Me.btnPay)
+        Me.GroupBox1.Location = New System.Drawing.Point(357, 38)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(313, 200)
+        Me.GroupBox1.TabIndex = 8
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Car Parking System"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.txtmarks)
+        Me.GroupBox2.Controls.Add(Me.btnPassOrFail)
+        Me.GroupBox2.Location = New System.Drawing.Point(25, 38)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(239, 228)
+        Me.GroupBox2.TabIndex = 9
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Pass Or Fail "
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label9)
+        Me.GroupBox3.Controls.Add(Me.Button1)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.lblname)
+        Me.GroupBox3.Controls.Add(Me.txtname)
+        Me.GroupBox3.Controls.Add(Me.cmbMonth)
+        Me.GroupBox3.Controls.Add(Me.cmbDAyWeek)
+        Me.GroupBox3.Location = New System.Drawing.Point(755, 38)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(266, 288)
+        Me.GroupBox3.TabIndex = 10
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "My BirthDay"
+        '
+        'cmbDAyWeek
+        '
+        Me.cmbDAyWeek.FormattingEnabled = True
+        Me.cmbDAyWeek.Items.AddRange(New Object() {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"})
+        Me.cmbDAyWeek.Location = New System.Drawing.Point(124, 85)
+        Me.cmbDAyWeek.Name = "cmbDAyWeek"
+        Me.cmbDAyWeek.Size = New System.Drawing.Size(121, 21)
+        Me.cmbDAyWeek.TabIndex = 0
+        '
+        'cmbMonth
+        '
+        Me.cmbMonth.FormattingEnabled = True
+        Me.cmbMonth.Items.AddRange(New Object() {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"})
+        Me.cmbMonth.Location = New System.Drawing.Point(124, 117)
+        Me.cmbMonth.Name = "cmbMonth"
+        Me.cmbMonth.Size = New System.Drawing.Size(121, 21)
+        Me.cmbMonth.TabIndex = 1
+        '
+        'txtname
+        '
+        Me.txtname.Location = New System.Drawing.Point(124, 52)
+        Me.txtname.Name = "txtname"
+        Me.txtname.Size = New System.Drawing.Size(100, 20)
+        Me.txtname.TabIndex = 2
+        '
+        'lblname
+        '
+        Me.lblname.AutoSize = True
+        Me.lblname.Location = New System.Drawing.Point(26, 59)
+        Me.lblname.Name = "lblname"
+        Me.lblname.Size = New System.Drawing.Size(90, 13)
+        Me.lblname.TabIndex = 3
+        Me.lblname.Text = "enter Your Name "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 88)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(88, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Day of the week "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(47, 120)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(37, 13)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Month"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(82, 151)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "My Birthday "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Button2)
+        Me.GroupBox4.Controls.Add(Me.txtDepth)
+        Me.GroupBox4.Controls.Add(Me.txtwidth)
+        Me.GroupBox4.Controls.Add(Me.txtheight)
+        Me.GroupBox4.Controls.Add(Me.Label8)
+        Me.GroupBox4.Controls.Add(Me.Label7)
+        Me.GroupBox4.Controls.Add(Me.Label6)
+        Me.GroupBox4.Controls.Add(Me.Label5)
+        Me.GroupBox4.Location = New System.Drawing.Point(1104, 38)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(200, 274)
+        Me.GroupBox4.TabIndex = 11
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Water Tank Capacity"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(6, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(175, 20)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Water Tank Capacity"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(18, 59)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 13)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "height (cm)"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(18, 88)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(52, 13)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "width(cm)"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(18, 120)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(59, 13)
+        Me.Label8.TabIndex = 3
+        Me.Label8.Text = "Depth (cm)"
+        '
+        'txtheight
+        '
+        Me.txtheight.Location = New System.Drawing.Point(83, 52)
+        Me.txtheight.Name = "txtheight"
+        Me.txtheight.Size = New System.Drawing.Size(74, 20)
+        Me.txtheight.TabIndex = 4
+        '
+        'txtwidth
+        '
+        Me.txtwidth.Location = New System.Drawing.Point(83, 85)
+        Me.txtwidth.Name = "txtwidth"
+        Me.txtwidth.Size = New System.Drawing.Size(74, 20)
+        Me.txtwidth.TabIndex = 5
+        '
+        'txtDepth
+        '
+        Me.txtDepth.Location = New System.Drawing.Point(83, 120)
+        Me.txtDepth.Name = "txtDepth"
+        Me.txtDepth.Size = New System.Drawing.Size(74, 20)
+        Me.txtDepth.TabIndex = 6
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(44, 167)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(113, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Calculate Capacity "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(27, 26)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(181, 20)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "When is your birthday"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, CType(((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic) _
+                Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label10.Location = New System.Drawing.Point(70, 16)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(154, 18)
+        Me.Label10.TabIndex = 8
+        Me.Label10.Text = "Car parking system"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1337, 450)
+        Me.Controls.Add(Me.GroupBox4)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.GroupBox4.ResumeLayout(False)
+        Me.GroupBox4.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents txtmarks As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnPassOrFail As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtPeriodTime As TextBox
+    Friend WithEvents btnPay As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblname As Label
+    Friend WithEvents txtname As TextBox
+    Friend WithEvents cmbMonth As ComboBox
+    Friend WithEvents cmbDAyWeek As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox4 As GroupBox
+    Friend WithEvents txtheight As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtDepth As TextBox
+    Friend WithEvents txtwidth As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label9 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,96 @@
+﻿Public Class Form1
+    Private Sub btnPassOrFail_Click(sender As Object, e As EventArgs) Handles btnPassOrFail.Click
+        Dim marks As Double
+
+
+        marks = txtmarks.Text
+        If marks < 39 Then
+            MsgBox("Failed")
+        ElseIf marks >= 40 And marks <= 49 Then
+            MsgBox("Supplimentary")
+        ElseIf marks >= 50 And marks <= 75 Then
+            MsgBox("Pass")
+        ElseIf marks > 76 And marks <= 100 Then
+            MsgBox("Distinction")
+        End If
+
+    End Sub
+
+    Private Sub btnPay_Click(sender As Object, e As EventArgs) Handles btnPay.Click
+        Dim Timespent As Double
+
+
+        Timespent = txtPeriodTime.Text
+
+        If Timespent >= 0 And Timespent <= 29 Then
+            MessageBox.Show("Your packed for " & "" & Timespent & "" & "Mins you must pay: R0")
+
+        ElseIf Timespent >= 30 And Timespent <= 59 Then
+            MessageBox.Show("Your packed for " & "" & Timespent & "" & "Mins you must pay: R6.50
+")
+        ElseIf Timespent >= 60 And Timespent <= 89 Then
+            MessageBox.Show("Your packed for " & "" & Timespent & "" & "Mins you must pay: R10.50")
+        ElseIf Timespent >= 90 Then
+            MessageBox.Show("Your packed for " & "" & Timespent & "" & "Mins you must pay: R250")
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name As String
+        Dim weeks As String
+        Dim month As String
+        name = txtname.Text
+        weeks = cmbDAyWeek.Text
+        month = cmbMonth.Text
+
+        If cmbDAyWeek.Text = "Monday" Then
+            MsgBox("Hello " & name & " Your birthday is " & "" & month & "and the day of the birthday this year is " & "" & weeks)
+
+
+        ElseIf cmbDAyWeek.Text = "Tuesday" Then
+            MsgBox("Hello " & name & "" & " Your birthday is " & " " & month & " " & "and the day of the birthday this year is " & " " & weeks)
+        ElseIf cmbDAyWeek.Text = "Wednesday" Then
+            MsgBox("Hello " & name & " " & " Your birthday is " & " " & month & " " & " and the day of the birthday this year is " & "" & weeks)
+        ElseIf cmbDAyWeek.Text = "Thursday" Then
+            MsgBox("Hello " & name & " " & " Your birthday is " & " " & month & " " & " and the day of the birthday this year is " & "" & weeks)
+        ElseIf cmbDAyWeek.Text = "Friday" Then
+            MsgBox("Hello " & name & " Your birthday is " & " " & month & " " & "and the day of the birthday this year is " & "" & weeks)
+        ElseIf cmbDAyWeek.Text = "Saturday" Then
+            MsgBox("Hello " & name & " Your birthday is " & " " & month & " " & "and the day of the birthday this year is " & "" & weeks)
+        ElseIf cmbDAyWeek.Text = "Sunday" Then
+            MsgBox("Hello " & name & " Your birthday is " & " " & month & " " & "and the day of the birthday this year is " & "" & weeks)
+        End If
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim height As Double
+        Dim width As Double
+        Dim depth As Double
+        Dim volume As Double
+
+        height = txtheight.Text
+        width = txtwidth.Text
+        depth = txtDepth.Text
+        volume = (height * width * depth) / 1000
+
+        MsgBox("The tank holds " & " " & volume & " litres of water ")
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        txtPeriodTime.Clear()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Dim form As New Form
+
+        Me.Close()
+    End Sub
+End Class
